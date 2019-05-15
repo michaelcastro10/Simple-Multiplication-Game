24ee8d1a2c60a192a24220d53b858a8e482367fa
michaelcastro10 <42814726+michaelcastro10@users.noreply.github.com>
2019-05-14 23:47:52 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,82 +1,57 @@
-//javascript.js
 var playing = false;
 var score;
 var action;
 var timeremaining;
 var correctAnswer;
 
-//if we click on the start/reset
 document.getElementById("startreset").onclick = function(){
 
-    //if we are playing
 
     if(playing == true){
 
-        location.reload(); //reload page
+        location.reload();
 
-    }else{//if we are not playing
-
-        //change mode to playing
+    }else{
 
         playing = true;
-
-        //set score to 0
 
         hide("rules")
 
         score = 0;
         document.getElementById("scorevalue").innerHTML = score;
 
-
-        //show countdown box
-
         show("timeremaining");
         timeremaining = 60;
         document.getElementById("timeremainingvalue").innerHTML = timeremaining;
 
-        //hide game over box
-
         hide("gameOver");
 
-        //change button to reset
         document.getElementById("startreset").innerHTML = "Reset Game";
 
-        //start countdown
-
         startCountdown();
-
-        //generate a new Q&A
 
         generateQA();
     }
 
 }
 
-//Clicking on an answer box
 
 for(i=1; i<5; i++){
     document.getElementById("box"+i).onclick = function(){
-    //check if we are playing
-    if(playing == true){//yes
+    if(playing == true){
         if(this.innerHTML == correctAnswer){
-        //correct answer
-
-            //increase score by 1
             score++;
             document.getElementById("scorevalue").innerHTML = score;
-            //hide wrong box and show correct box
             hide("wrong");
             show("correct");
             setTimeout(function(){
                 hide("correct");
             }, 1000);
-            if (score >=15){
+            if (score >=10){
               show("winner")
               document.getElementById("winner").innerHTML = "<p>Congrats!</p><p>You won.</p>";
               hide("timeremaining");
             }
-
-            //Generate new Q&A
 
             generateQA();
         }else{
@@ -90,30 +65,13 @@ for(i=1; i<5; i++){
     }
 }
 }
-//if we click on answer box
-    //if we are playing
-        //correct?
-            //yes
-                //increase score
-                //show correct box for 1sec
-                //generate new Q&A
-            //no
-                //show try again box for 1sec
-                //if(score==5){
-                //  show ("winner");
-                //  document.getElementById("winnder").innerHTML = "<p>Game over!</p><p>Congrats! You win.</p>";
-              //  }
 
-
-//functions
-
-//start counter
 
 function startCountdown(){
     action = setInterval(function(){
         timeremaining -= 1;
         document.getElementById("timeremainingvalue").innerHTML = timeremaining;
-        if(timeremaining == 0){// game over
+        if(timeremaining == 0){
             stopCountdown();
             show("gameOver");
          document.getElementById("gameOver").innerHTML = "<p>Game over!</p><p>Your score is " + score + ".</p>";
@@ -127,25 +85,21 @@ function startCountdown(){
 
 }
 
-//stop counter
+
 
 function stopCountdown(){
     clearInterval(action);
 }
 
-//hide an element
 
 function hide(Id){
     document.getElementById(Id).style.display = "none";
 }
 
-//show an element
 
 function show(Id){
     document.getElementById(Id).style.display = "block";
 }
-
-//generate question and multiple answers
 
 function generateQA(){
     var x = 1+ Math.round(20*Math.random());
@@ -153,9 +107,7 @@ function generateQA(){
     correctAnswer = x*y;
     document.getElementById("question").innerHTML = x + "x" + y;
     var correctPosition = 1+ Math.round(3*Math.random());
-    document.getElementById("box"+correctPosition).innerHTML = correctAnswer; //fill one box with the correct answer
-
-    //fill other boxes with wrong answers
+    document.getElementById("box"+correctPosition).innerHTML = correctAnswer; 
 
     var answers = [correctAnswer];
 
@@ -163,7 +115,7 @@ function generateQA(){
         if(i != correctPosition) {
             var wrongAnswer;
             do{
-                wrongAnswer = (1+ Math.round(20*Math.random()))*(1+ Math.round(20*Math.random())); //a wrong answer
+                wrongAnswer = (1+ Math.round(20*Math.random()))*(1+ Math.round(20*Math.random())); 
             }while(answers.indexOf(wrongAnswer)>-1)
             document.getElementById("box"+i).innerHTML = wrongAnswer;
             answers.push(wrongAnswer);
